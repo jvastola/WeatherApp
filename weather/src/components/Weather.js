c158865d4837c3d91f5ce993e6af8d4c62d98036
jvastola <jvastola@ucmerced.edu>
2021-01-05 00:25:42 -0800

    initial commit

---
+++ weather/src/components/Weather.js
@@ -0,0 +1,15 @@
+import {React, Component} from 'react'
+
+class Weather extends Component{
+    constructor(props){
+        super()
+    }
+    render() {
+        return(
+            <div>
+                Weather
+            </div>
+        )
+    }
+}
+export default Weather
