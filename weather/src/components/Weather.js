2ae75027ec8ee01874e4d15e85d6b84e6b64887d
jvastola <jvastola@ucmerced.edu>
2021-01-05 03:33:31 -0800

    api access

--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -1,13 +1,27 @@
 import {React, Component} from 'react'
-
+import Moment from "moment"
+require('dotenv').config()
 class Weather extends Component{
     constructor(props){
         super()
     }
+  fetchWeather = async e => {
+    const zip = e.target.elements.zip.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=${process.env.API_KEY}&units=imperial`
+    );
+
+    const data = await api_call.json();
+    console.log(data)
+  }
     render() {
+        const date = new Date()
         return(
             <div>
-                Weather
+                <p>{[date]}
+                </p>
+                Weather 
             </div>
         )
     }
