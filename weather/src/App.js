1a18ac06fdda3a31ff8dfe460fc49d9c011fe8ce
jvastola <jvastola@ucmerced.edu>
2021-01-22 01:10:17 -0800

    App modifications

--- weather/src/App.js
+++ weather/src/App.js
@@ -4,18 +4,18 @@ import Weather from './components/Weather'
 import Form from './components/Form'
 export default class App extends Component {
   constructor() {
+    super()
     this.state ={
       zip:""
     }
   }
   getWeather = async e => {
     e.preventDefault();
-    const zip-1 = e.target.elements.zip1.value;
-    const zip-2 = e.target.elements.zip2.value;
-    const zip-3 = e.target.elements.zip3.value;
-    const zip-4 = e.target.elements.zip4.value;
-    const zip-5 = e.target.elements.zip5.value;
-    const zip = e.target.elements.zip.value;
+    const zip1 = e.target.elements.zip1.value;
+    const zip2 = e.target.elements.zip2.value;
+    const zip3 = e.target.elements.zip3.value;
+    const zip4 = e.target.elements.zip4.value;
+    const zip5 = e.target.elements.zip5.value;
   }
   if(zip){
     this.setState({zip:zip})
@@ -24,8 +24,7 @@ export default class App extends Component {
     
     return (
       <div className="App">
-        <Form  getWeather={this.getWeather}/>
-        <Weather zip= {this.state.zip} />
+        <Weather  />
       </div>
     );
   }
