ad0e30165940f63bbb4079c3aaa09c0d4506c67e
jvastola <jvastola@ucmerced.edu>
2021-01-22 01:10:03 -0800

    Weather modifications

--- weather/src/components/Weather.js
+++ weather/src/components/Weather.js
@@ -1,27 +1,29 @@
 import {React, Component} from 'react'
-import Moment from "moment"
-require('dotenv').config()
+import Card from "./Card"
+require('dotenv').config();
+
 class Weather extends Component{
     constructor(props){
         super()
+        this.state = {
+          fullData: [],
+          dailyData: []
+        }
     }
-  fetchWeather = async e => {
-    const zip = e.target.elements.zip.value;
-    e.preventDefault();
-    const api_call = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=${process.env.API_KEY}&units=imperial`
-    );
-
-    const data = await api_call.json();
-    console.log(data)
-  }
+    componentDidMount(){
+      const zip = "11102"
+      const weatherLink = `https://api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`
+      fetch(weatherLink)
+        .then(res=>res.json())
+        .then(data => console.log("Data List Loaded", data.list))
+    }
+    formatCards = () => {
+      return this.state.dailyData.map((reading, index) => <Card reading={reading} key={index} />)
+    }
     render() {
-        const date = new Date()
         return(
             <div>
-                <p>{[date]}
-                </p>
-                Weather 
+                {this.formatCards()}
             </div>
         )
     }
